b742968e557882d0381096985d4e202dd9c48a8c
dwillcox <eugene.willcox@gmail.com>
2017-06-29 15:46:30 -0400

    Added setup scripts for the DDT and Hybrid-DDT Flash simulations.

---
+++ flash-setup-scripts/setup_snia_ddt.sh
@@ -0,0 +1,1 @@
+./setup SNIa_ddt -objdir=build/object_snia_ddt -2d -auto -nxb=16 -nyb=16 +cylindrical -maxblocks=10000 -with-unit=Particles/ParticlesMain 
---
+++ flash-setup-scripts/setup_snia_hddt.sh
@@ -0,0 +1,1 @@
+./setup SNIa_hddt -objdir=build/object_snia_hddt -2d -auto -nxb=16 -nyb=16 +cylindrical -maxblocks=10000 -with-unit=Particles/ParticlesMain 
